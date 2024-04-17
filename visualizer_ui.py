f358aa3dc1c13496270d733ab73779c78e31966f
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-04-17 11:01:55 +0700

    modified the flight search page of the notebook

--- visualizer_ui.py
+++ visualizer_ui.py
@@ -1,11 +1,16 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, font
 
 
 class VisualizerUI(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Indian flight visualizer")
+        self.from_city = tk.StringVar()
+        self.to_city = tk.StringVar()
+        self.flight = tk.StringVar()
+        self.default_font = font.nametofont("TkDefaultFont")
+        self.default_font.configure(family="Times", size=15)
         self.init_notebook()
 
     def init_components(self):
@@ -15,10 +20,39 @@ class VisualizerUI(tk.Tk):
         notebook = ttk.Notebook(self)
         notebook.pack(pady=10, expand=True, anchor=tk.N, fill="both")
         names = ["Flight search", "Flight planner", "Data summary"]
-        for i in range(3):
-            page = tk.Frame(notebook, width=400, height=400)
-            page.pack(fill='both', expand=True)
-            notebook.add(page, text=names[i])
+        notebook.add(self.init_flight_search(), text=names[0])
+        # for i in range(3):
+        #     page = tk.Frame(notebook, width=400, height=400)
+        #     page.pack(fill='both', expand=True)
+        #     notebook.add(page, text=names[i])
+
+    def init_flight_search(self):
+        mainframe = tk.Frame(self)
+        frame1 = tk.Frame(mainframe)
+        frame2 = tk.Frame(mainframe)
+        frame3 = tk.Frame(mainframe)
+        text_sum = tk.Label(frame3, text="Price analysis", anchor=tk.N)
+        text_sum.pack(expand=True, fill='both')
+        placeholder = tk.Text(frame2)
+        placeholder.pack(expand=True, fill="both")
+        from_label = tk.Label(frame1, text="From:")
+        from_combo = ttk.Combobox(frame1, textvariable=self.from_city)
+        to_label = tk.Label(frame1, text="To:")
+        to_combo = ttk.Combobox(frame1, textvariable=self.to_city)
+        flight_label = tk.Label(frame1, text="Flight:")
+        flight_combo = ttk.Combobox(frame1, textvariable=self.flight)
+        settings = {"padx":10, "pady":5, "anchor":tk.W,"expand":True, "fill":"y"}
+        from_label.pack(**settings)
+        from_combo.pack(**settings)
+        to_label.pack(**settings)
+        to_combo.pack(**settings)
+        flight_label.pack(**settings)
+        flight_combo.pack(**settings)
+        frame1.pack(fill="both", expand=True, side=tk.LEFT)
+        frame2.pack(fill="both", expand=True, side=tk.LEFT)
+        frame3.pack(fill="both", expand=True, side=tk.RIGHT)
+        mainframe.pack(fill='both', expand=True)
+        return mainframe
 
     def run(self):
         self.mainloop()
