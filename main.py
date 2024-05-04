60d1d132d66937d37c1dcbea21cf4e511eb20364
“Khunakorn Pattayakorn” <khunakornp@gmail.com>
2024-05-04 13:40:35 +0700

    Moved the dataset into a folder

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pandas as pd
 from visualizer_ui import VisualizerUI
 from model_logic import DataframeLogic
 from controllers import Controller
+import os
 
 
 if __name__ == "__main__":
-    orig_df = pd.read_csv("Indian Airlines.csv")
+    orig_df = pd.read_csv(os.path.join(os.getcwd(), "Datasets",
+                                       "Indian Airlines.csv"))
     model = DataframeLogic(orig_df)
     ui = VisualizerUI()
     controller = Controller(ui, model)
